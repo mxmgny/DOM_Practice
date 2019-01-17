96b1ae8e68f5c807e81e48cab030cefe184309b9
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-01-17 21:06:49 +0200

    Json load from JS

--- script/script.js
+++ script/script.js
@@ -1,11 +1,29 @@
 
-const requestURL = "A:/coding/kottans/DOM_Practice/script/data.json/";
+const requestURL = "https://raw.githubusercontent.com/mxmgny/DOM_Practice/master/script/data.json";
 const request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
+const section = document.querySelector('section');
+
 request.onload = function() {
-    var obj = request.response;
-    console.log(obj);
+    var races = request.response;
+    console.log(races['items']);
+    for (let i = 0; i < races['items'].length; i++) {
+        var race = races['items'][i];
+        var article = document.createElement('article');
+        var header = document.createElement('h2');
+        var paragraph = document.createElement('p');
+        var img = document.createElement('img');
+        
+
+        header.textContent = race['header'];
+        paragraph.textContent = race['decription-text'];
+        img.src = "https://raw.githubusercontent.com/mxmgny/DOM_Practice/master/"+race['logo'];
+        article.appendChild(header);
+        article.appendChild(img);
+        article.appendChild(paragraph);
+        section.appendChild(article);
+    }
 }
