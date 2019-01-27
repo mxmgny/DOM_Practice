7272b2d0a95e00d488d3c97af57c0bfdaa1f95d2
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-01-28 01:35:03 +0200

    Deleted comments and outline on butttons

--- script/script.js
+++ script/script.js
@@ -1,17 +1,13 @@
-
 const requestURL = "https://raw.githubusercontent.com/mxmgny/DOM_Practice/master/script/data.json";
 const request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
-// element for sidebar links
 const aside = document.createElement('aside');
-//create element for main content
 const main = document.createElement('main');
 const body = document.querySelector('body');
 
-//call loadPage function and throw JSON object as argument
 request.onload = function() {
     loadPage(request.response);
 }
@@ -20,14 +16,14 @@ function loadPage(jsonObj) {
     const races = jsonObj['items'];
 
     races.map( (race,id) => {
-//      create navButton and add Event listener
+
         const navButton = createTextElement('button',race['header']);
         navButton.addEventListener('click', changeRace);
         if(id == 0) navButton.classList.add('active');
-//      create article
+
         const raceArticle = createRaceArticle(race);
         if(id == 0) raceArticle.classList.add('active');
-//      add navButton and main to the DOM
+
         aside.appendChild(navButton);
         main.appendChild(raceArticle);
     });
@@ -35,7 +31,6 @@ function loadPage(jsonObj) {
     body.appendChild(aside);
     body.appendChild(main);
 }
-
 
 function createRaceArticle(raceInfo) {
     let article = document.createElement('article');
@@ -45,6 +40,7 @@ function createRaceArticle(raceInfo) {
      article.appendChild(createTextElement('p',raceInfo['decription-text']));
      return article;
 }
+
 function createImage(src) {
     let imgElement = document.createElement('img');
     imgElement.src = "https://raw.githubusercontent.com/mxmgny/DOM_Practice/master/"+src;
@@ -57,21 +53,14 @@ function createTextElement(elementTag, text){
     return element;
 }
 
-
-/*Selects all elements by given element name and clear its' class list*/
 function deactivate(tagName) { 
     let arrayOfElements = Array.from(document.getElementsByTagName(tagName));
     arrayOfElements.map((element) => {element.classList.value = ""});
 }
 
-
-/*Delete all classes from sidebar elements and sections
-  Add .active class to chosen section and sidebar button*/
 function changeRace() {
     deactivate('article');
     deactivate('button');
     document.getElementById(this.innerText).classList.add('active');
     this.classList.add('active');
 }
-
-
